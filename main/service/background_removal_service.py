f251240181449e3583f76a27588e8d06445f100c
i-zanis <ioanlafazanis@gmail.com>
2023-04-19 08:05:15 +0100

    refactor: Replace subprocess with rembg library in background removal service

--- main/service/background_removal_service.py
+++ main/service/background_removal_service.py
@@ -1,31 +1,41 @@
 import shutil
-import subprocess
 import tempfile
 from pathlib import Path
 
 from fastapi import UploadFile
+from rembg import remove
 
 
 async def remove_background(file: UploadFile):
     try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=file.filename) \
-                as temp_file:
-            temp_path = Path(temp_file.name)
-            shutil.copyfileobj(file.file, temp_file)
-
-        process = subprocess.run(["rembg", "i", str(temp_path)],
-                                 capture_output=True)
-
-        has_processed_failed = process.returncode != 0
-        if has_processed_failed:
+        temp_path = await create_temp_input_file(file)
+        input_data = await read_input_file(temp_path)
+        output_data = remove(input_data)
+        if output_data is None:
             return {"success": False}
+        output_file_path = await create_temp_output_file(output_data)
+        return {"success": True, "file_path": output_file_path}
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") \
-                as output_temp_file:
-            output_temp_file.write(process.stdout)
+    except (Exception) as e:
+        return {"success": False, "error": str(e)}
 
-        return {"success": True, "file_path": output_temp_file.name}
 
-    except Exception as e:
-        print(f"Error in remove_background: {e}")
-        return {"success": False}
+async def create_temp_input_file(file: UploadFile):
+    with tempfile.NamedTemporaryFile(delete=False,
+                                     suffix=file.filename) as temp_file:
+        temp_path = Path(temp_file.name)
+        shutil.copyfileobj(file.file, temp_file)
+    return temp_path
+
+
+async def read_input_file(temp_path: Path):
+    with open(temp_path, 'rb') as input_file:
+        input_data = input_file.read()
+    return input_data
+
+
+async def create_temp_output_file(output_data: bytes):
+    with tempfile.NamedTemporaryFile(delete=False,
+                                     suffix=".png") as output_temp_file:
+        output_temp_file.write(output_data)
+    return output_temp_file.name
