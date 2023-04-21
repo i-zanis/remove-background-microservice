62ae97cb5134463076e03a8bbacf07dd7206f704
i-zanis <ioanlafazanis@gmail.com>
2023-04-21 08:37:54 +0100

    test: fix tests requiring annotation

--- test/service_test.py
+++ test/service_test.py
@@ -10,23 +10,26 @@ def test_image():
         yield UploadFile(f)
 
 
+@pytest.mark.asyncio
 async def test_remove_background_success(test_image):
     result = await remove_background(test_image)
-    assert result["success"] == True
+    assert result["success"] is True
     assert "file_path" in result
 
 
+@pytest.mark.asyncio
 async def test_remove_background_failure(test_image):
     with open("test_image.txt", "rb") as f:
         wrong_file = UploadFile(f)
         result = await remove_background(wrong_file)
-        assert result["success"] == False
+        assert result["success"] is False
 
 
+@pytest.mark.asyncio
 async def test_remove_background_exception(test_image, monkeypatch):
     def raise_error(*args, **kwargs):
         raise Exception("Test Exception")
 
     monkeypatch.setattr("subprocess.run", raise_error)
     result = await remove_background(test_image)
-    assert result["success"] == False
+    assert result["success"] is False
